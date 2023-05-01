85e04ea86ead158fbc4829ba62dfbcd5f4854e2f
fneyron <florent.neyron@gmail.com>
2023-05-01 09:24:11 +0200

    Adding notification to forms

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
+from babel.dates import format_datetime
 from flask import url_for
 from flask_babel import _
 from flask_login import current_user
-from babel.dates import format_datetime
+from sqlalchemy import event
+from sqlalchemy.orm import Session
+
 from apps import db
 
 
@@ -15,11 +18,17 @@ class Attendee(db.Model):
     email = db.Column(db.String(64), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
 
-    def __init__(self, email, event_id, user_id=None):
+    def __init__(self, email, event, user_id=None):
         self.email = email
-        self.event_id = event_id
+        self.event_id = event.id
         self.user_id = user_id
 
+    @property
+    def notification_message(self):
+        event = Event.query.get(self.event_id)
+        event_title = event.new_summary
+        calendar_name = event.calendar.name
+        return f"<b>{current_user.firstname} {current_user.lastname}</b> invited you to the following event '{calendar_name} : {event_title}'"
 
 class Event(db.Model):
     __tablename__ = 'Event'
@@ -32,7 +41,7 @@ class Event(db.Model):
     new_description = db.Column(db.String)
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
-    last_modified = db.Column(db.DateTime, onupdate=datetime.utcnow(), default=datetime.utcnow())
+    last_modified = db.Column(db.DateTime, onupdate=datetime.utcnow, default=datetime.utcnow)
     all_day = db.Column(db.Boolean(), default=False)
     calendar_id = db.Column(db.Integer, db.ForeignKey('Calendar.id'))
     ical_id = db.Column(db.Integer, db.ForeignKey('ICal.id'))
@@ -40,44 +49,47 @@ class Event(db.Model):
     creator_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
 
     def add_attendee(self, email):
-        from apps.tasks import send_email
-        from apps.authentication.models import Users
-
-        # Check if the user already exist
+        from apps.authentication.models import Users, Notification
+        # Check if the user already exists and if an invitation was already sent for this email
         user = Users.query.filter_by(email=email).first()
+        existing_invitation = Attendee.query.filter_by(email=email, user_id=None).first()
 
-        # Check if an invitation was already sent for this email
-        send_invitation = not (Attendee.query.filter_by(email=email, user_id=None).first())
-
-        attendee = Attendee(email=email, event_id=self.id)
+        attendee = Attendee(email=email, event=self)
         db.session.add(attendee)
+
+        if not user and not existing_invitation:
+            self.send_invitation_email(email)
+        elif user:
+            self.associate_attendee_with_user(user)
+            if user.notification_settings.event_notification_email:
+                self.send_invitation_email(email)
+
+        notification = Notification(content=attendee.notification_message, user=current_user)
+        db.session.add(notification)
         db.session.commit()
 
-        if not user:
-            # Send invitation if no previous invitation was sent for this email
-            if send_invitation:
-                lang_code = current_user.language
-                send_email.delay(
-                    recipients=[email],
-                    lang_code=lang_code,
-                    subject=_("You've been invited to an event"),
-                    text=_("You've been invited to an event by %s %s. Please sign up to view the details: %s" % (
-                        current_user.firstname, current_user.lastname,
-                        url_for('home_blueprint.index', _external=True))),
-                    template='email/event_email_template.html',
-                    content=_(
-                        'You are receiving this email because %s, invited you to the following event.') %
-                            (current_user.firstname),
-                    event={
-                        'title': self.new_summary,
-                        'start': format_datetime(self.start_time, format='medium', locale=lang_code),
-                        'end': format_datetime(self.end_time, format='medium', locale=lang_code),
-                    },
-                    buttons={'url': url_for('home_blueprint.index', _external=True), 'text': _('View Event')},
-                )
-        else:
-            self.associate_attendee_with_user(user)
-
+    def send_invitation_email(self, email):
+        from apps.tasks import send_email
+        lang_code = current_user.language
+        send_email.delay(
+            recipients=[email],
+            lang_code=lang_code,
+            subject=_("You've been invited to an event"),
+            text=_("You've been invited to an event by %s %s. Please sign-in or register to view the details: %s" % (
+                current_user.firstname, current_user.lastname,
+                url_for('home_blueprint.index', _external=True))),
+            template='email/event_email_template.html',
+            content=_(
+                'You are receiving this email because %s, invited you to the following event.') %
+                    (current_user.firstname),
+            event={
+                'calendar': self.calendar.name,
+                'title': self.new_summary,
+                'start': format_datetime(self.start_time, format='d MMM YYYY' if self.all_day else 'medium', locale=lang_code),
+                'end': format_datetime(self.end_time, format='d MMM YYYY' if self.all_day else 'medium', locale=lang_code),
+            },
+            buttons={'url': url_for('home_blueprint.index', _external=True), 'text': _('View Event')},
+        )
     def associate_attendee_with_user(self, user):
         attendees = Attendee.query.filter_by(email=user.email).all()
         for attendee in attendees:
